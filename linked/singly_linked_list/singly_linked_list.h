3fd2d6040d4fa675a2b8926d4cd031ff25c58a9a
Ranieri Althoff <ranisalt@gmail.com>
2014-06-05 03:18:51 -0300

    Updates to follow commit:8a81ac0 improvements, fixed segmentation faults on various methods and use more C++11 (notably swap operation)

--- linked/singly_linked_list/singly_linked_list.h
+++ linked/singly_linked_list/singly_linked_list.h
@@ -1,175 +1,225 @@
 #ifndef SINGLY_LINKED_LIST_H_
 #define SINGLY_LINKED_LIST_H_
 
+#include <algorithm>
 #include <stdexcept>
-#include "../../abstract/list.h"
+#include "abstract/list.h"
 
 namespace data_structures {
+namespace linked {
 
 using abstract::list;
 
-namespace linked {
-
 template<typename T>
 class singly_linked_list: public list<T> {
-protected:
+private:
 	struct node {
-		node(node* succ, const T& value) :
-				succ(succ), value(value) {
+	public:
+		node(node* succ, const T& item) :
+				_succ(succ), _item(item) {
 		}
 
-		node* succ;
-		T value;
+		node* _succ;
+		T _item;
 	};
 
 	template<typename NodeT>
 	class iterator_base {
 	public:
 		iterator_base(node* ptr) :
-				ptr(ptr) {
+				_ptr(ptr) {
 		}
 
-		iterator_base& operator=(const iterator_base& other) {
-			if (!this == other)
-				return this(other);
-			else
-				return this;
+		iterator_base operator=(const iterator_base& other) {
+			return {other._ptr};
 		}
 
 		iterator_base& operator++() {
-			this->ptr = this->ptr->succ;
+			if (!_ptr)
+				throw std::out_of_range("Iterating beyond list end.");
+			_ptr = _ptr->_succ;
 			return *this;
 		}
 
-		iterator_base& operator++(int) {
-			iterator_base other(this);
-			++this;
-			return other;
+		iterator_base operator++(int) {
+			node* old = _ptr;
+			++(*this);
+			return {old};
 		}
 
 		bool operator==(const iterator_base& other) const {
-			return this->ptr == other.ptr;
+			return _ptr == other._ptr;
 		}
 
 		bool operator!=(const iterator_base& other) const {
-			return !(*this == other);
+			return _ptr != other._ptr;
 		}
 
 		NodeT& operator*() const {
-			return this->ptr->value;
+			return _ptr->_item;
 		}
 
 		NodeT* operator->() const {
-			return this->ptr->value;
+			return &(_ptr->_item);
 		}
 
 	private:
-		node* ptr;
+		node* _ptr;
 	};
 
-	node* head;
+	using parent = list<T>;
+	using self = singly_linked_list<T>;
 
 public:
-	using list<T>::empty_check;
+	singly_linked_list() :
+			_head(), _size() {
+	}
+
+	singly_linked_list(const parent& other) {
+		for (auto e : other)
+			push_back(e);
+	}
+
+	~singly_linked_list() {
+		node* old;
+		while (_head) {
+			old = _head;
+			_head = _head->_succ;
+			delete old;
+		}
+	}
+
+	T at(int position) const {
+		if (position < 0 || position >= this->_size)
+			throw std::out_of_range("Out of range access.");
+
+		node* p = _head;
+		for (int i = 0; i < position; ++i)
+			p = p->_succ;
+		return p->_item;
+	}
 
 	T back() const {
 		empty_check();
 
-		node* p = head;
-		while (p->succ != 0)
-			p = p->succ;
-		return p->value;
+		node* p = _head;
+		while (p->_succ != 0)
+			p = p->_succ;
+		return p->_item;
 	}
 
 	T front() const {
 		empty_check();
 
-		return head->value;
+		return _head->_item;
+	}
+
+	int size() const {
+		return this->_size;
 	}
 
 	/**< Removal operations */
 	T pop(int position) {
-		if (position < 0 && position >= this->_length)
+		if (position < 0 || position >= this->_size)
 			throw std::out_of_range("Empty list.");
 
 		if (position == 0)
 			return pop_front();
 
-		node* p = head;
+		node* p = _head;
 		for (int i = 1; i < position; ++i)
-			p = p->succ;
+			p = p->_succ;
 
 		/**< Hold node, advance it and then delete the old one */
-		node* aux = p->succ;
-		T value(aux->value);
-		p->succ = aux->succ;
+		node* aux = p->_succ;
+		T value(aux->_item);
+		p->_succ = aux->_succ;
 		delete aux;
 
-		--this->_length;
+		--this->_size;
 		return value;
 	}
 
 	T pop_back() {
-		return pop(this->_length - 1);
+		return pop(this->_size - 1);
 	}
 
 	T pop_front() {
 		empty_check();
 
 		/**< Hold head, advance it and then delete the old one */
-		node* aux = head;
-		T value(aux->value);
-		head = aux->succ;
+		node* aux = _head;
+		T value(aux->_item);
+		_head = aux->_succ;
 		delete aux;
 
-		--this->_length;
+		--this->_size;
 		return value;
 	}
 
 	/**< Insertion operations */
-	void push(int position, const T& value) {
-		if (position < 0 && position > this->_length)
-			throw std::out_of_range("Empty list.");
+	void push(int position, const T& item) {
+		if (position < 0 || position > this->_size)
+			throw std::out_of_range("Out of range access.");
 
 		if (position == 0) {
-			push_front(value);
+			push_front(item);
 			return;
 		}
 
-		node* p = head;
+		node* p = _head;
 		for (int i = 1; i < position; ++i)
-			p = p->succ;
-		p->succ = new node(p->succ, value);
-		++this->_length;
+			p = p->_succ;
+		p->_succ = new node(p->_succ, item);
+		++this->_size;
 	}
 
 	void push_back(const T& value) {
-		push(this->_length, value);
+		push(this->_size, value);
 	}
 
 	void push_front(const T& value) {
-		head = new node(head, value);
-		++this->_length;
+		_head = new node(_head, value);
+		++this->_size;
 	}
 
-	typedef iterator_base<T> iterator;
-	typedef iterator_base<const T> const_iterator;
-
 	iterator begin() {
-		return iterator(head);
+		return {_head};
 	}
 
 	iterator end() {
-		return iterator(0);
+		return {0};
 	}
 
 	const_iterator begin() const {
-		return const_iterator(head);
+		return {_head};
 	}
 
 	const_iterator end() const {
-		return const_iterator(0);
+		return {0};
 	}
+
+	self& operator=(self rhs) {
+		swap(*this, rhs);
+		return *this;
+	}
+
+	friend void swap(self& a, self& b) {
+		using std::swap;
+
+		swap(a._head, b._head);
+		swap(a._size, b._size);
+	}
+
+private:
+	void empty_check() const {
+		if (!_size)
+			throw std::out_of_range("Empty list.");
+	}
+
+	node* _head { };
+	int _size { };
+
 };
 
 }
