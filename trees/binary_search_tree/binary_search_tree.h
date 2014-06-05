ee5919ecb6a9d73f94d6bb28f8be7779d139f73b
Ranieri Althoff <ranisalt@gmail.com>
2014-06-04 21:16:01 -0300

    Singly linked list fixed

---
+++ trees/binary_search_tree/binary_search_tree.h
@@ -0,0 +1,120 @@
+/*
+ * binary_search_tree.h
+ *
+ *  Created on: 30/05/2014
+ *      Author: ranieri
+ */
+
+#ifndef BINARY_SEARCH_TREE_H_
+#define BINARY_SEARCH_TREE_H_
+
+#include <ostream>
+#include <stdexcept>
+
+namespace data_structures { namespace trees {
+
+template<typename T>
+class binary_search_tree {
+	struct node {
+		node(const T& info) :
+				_item(info) {
+		}
+
+		friend std::ostream& operator<<(std::ostream& out, const node& node) {
+			if (node._left) {
+				out << *(node._left) << ' ';
+			}
+			out << node._item;
+			if (node._right) {
+				out << ' ' << *(node._right);
+			}
+			return out;
+		}
+
+		node* _left = 0;
+		node* _right = 0;
+		T _item;
+	};
+
+	node*& find_leftmost(node*& root) {
+		node* curr = root;
+		while (curr->_left) {
+			curr = curr->_left;
+		}
+		return curr;
+	}
+
+	void insert(node*& root, const T& item) {
+		if (!root) {
+			root = new node(item);
+		} else {
+			if (item < root->_item) {
+				insert(root->_left, item);
+			} else if (item > root->_item) {
+				insert(root->_right, item);
+			} else {
+				throw std::exception("Inserting element already existent.");
+			}
+		}
+	}
+
+	node* remove(node*& root, const T& item) {
+		if (!root)
+			return root;
+		if (item < root->_item) {
+			root->_left = remove(root->_left, item);
+			return root;
+		}
+		if (item > root->_item) {
+			root->_right = remove(root->_right, item);
+			return root;
+		}
+		if (root->_left && root->_right) {
+			node* succ = find_leftmost(root->_right);
+			root->_item = succ->_item;
+			root->_right = remove(root->_right, item);
+			return root;
+		}
+		if (root->_left)
+			return root->_left;
+		if (root->_right)
+			return root->_right;
+		delete root;
+		return 0;
+	}
+
+	node* root = 0;
+
+public:
+	binary_search_tree() {
+
+	}
+
+	bool empty() const {
+		return root == 0;
+	}
+
+	void pop(const T& item) {
+		remove(root, item);
+	}
+
+	void push(const T& item) {
+		insert(root, item);
+	}
+
+	friend std::ostream& operator<<(std::ostream& out,
+			const binary_search_tree& bst) {
+		if (bst.root->_left) {
+			out << *(bst.root->_left) << ' ';
+		}
+		out << bst.root->item;
+		if (bst.root->_right) {
+			out << ' ' << *(bst.root->_right);
+		}
+		return out;
+	}
+};
+
+}}
+
+#endif /* BINARY_SEARCH_TREE_H_ */
---
+++ trees/binary_search_tree/binary_search_tree_test.cpp
@@ -0,0 +1,18 @@
+/*
+ * binary_search_tree_test.cpp
+ *
+ *  Created on: 29/05/2014
+ *      Author: ranieri
+ */
+
+#include <gtest/gtest.h>
+#include "binary_search_tree.h"
+
+using data_structures::trees::binary_search_tree;
+
+class binary_search_tree_test: public testing::Test {
+public:
+	binary_search_tree<int> tree;
+};
+
+//TEST_F(binary_search_tree_test, )
