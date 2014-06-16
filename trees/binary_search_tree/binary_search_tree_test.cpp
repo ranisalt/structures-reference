4f459913071b9f96a697888125397062403840e8
Ranieri Althoff <ranisalt@gmail.com>
2014-06-15 23:28:21 -0300

    Added test to assert binary search tree is created empty.

--- trees/binary_search_tree/binary_search_tree_test.cpp
+++ trees/binary_search_tree/binary_search_tree_test.cpp
@@ -7,23 +7,30 @@
 
 #include <gtest/gtest.h>
 #include "binary_search_tree.h"
+#include "linked/doubly_linked_list/doubly_linked_list.h"
 
 using data_structures::trees::binary_search_tree;
+using data_structures::linked::doubly_linked_list;
 
 class binary_search_tree_test: public testing::Test {
 public:
 	binary_search_tree<int> tree;
 };
 
+TEST_F(binary_search_tree_test, isCreatedEmpty) {
+	doubly_linked_list<int> list;
+	EXPECT_EQ(list, tree.in_order());
+}
+
 TEST_F(binary_search_tree_test, pushPushes) {
 	tree.push(42);
-	EXPECT_TRUE(tree.find(42));
+	EXPECT_TRUE(tree.has(42));
 }
 
 TEST_F(binary_search_tree_test, popPops) {
 	tree.push(42);
 	tree.pop(42);
-	EXPECT_FALSE(tree.find(42));
+	EXPECT_FALSE(tree.has(42));
 }
 
 TEST_F(binary_search_tree_test, insertionsAreBalancedAsExpected) {
