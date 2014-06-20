c5f94eb3baae0918e1c276490faf39a3d64442e1
Ranieri Althoff <ranisalt@gmail.com>
2014-06-20 03:00:52 -0300

    Added comments to the AVL-tree. It is now stable, tested and leak-free.

--- trees/avl_tree/avl_tree.h
+++ trees/avl_tree/avl_tree.h
@@ -12,7 +12,7 @@ namespace trees {
 using abstract::tree;
 using linked::doubly_linked_list;
 
-template<typename T, template<typename > class Container = doubly_linked_list>
+template<typename T, template<typename> class Container = doubly_linked_list>
 class avl_tree: public tree<T, Container> {
 	using size_type = std::size_t;
 
@@ -39,8 +39,7 @@ private:
 	}
 
 	size_type factor(node* root) const {
-		return (root == nullptr) ?
-				0 : height(root->_left) - height(root->_right);
+		return (root == nullptr) ? 0 : height(root->_left) - height(root->_right);
 	}
 
 	size_type height(node* root) const {
@@ -48,26 +47,47 @@ private:
 	}
 
 	node* insert(node* root, const T& item) {
+		// If we find a null root, we found the right spot.
 		if (root == nullptr)
 			return root = new node(item);
+
+		// If root's value is greater than inserted value, try to insert to the left.
 		else if (root->_item > item) {
 			root->_left = insert(root->_left, item);
+
+			// If the factor of root unbalancing is 2, we have a left-left or left-right case.
 			if (factor(root) == 2) {
+
+				// If the factor of the left node is -1, we have a left-right case.
 				if (factor(root->_left) == -1)
 					rotate_left(root->_left);
+
+				// The tree is now guaranteedly a left-left case.
 				rotate_right(root);
 			}
-		} else if (root->_item < item) {
+		}
+		
+		// If root's value is lesser than inserted value, try to insert to the right.
+		else if (root->_item < item) {
 			root->_right = insert(root->_right, item);
+
+			// If the factor of root unbalancing is -2, we have a right-left or right-right case.
 			if (factor(root) == -2) {
+
+				// If the factor of the right node is 1, we have a right-left case.
 				if (factor(root->_right) == 1)
 					rotate_right(root->_right);
+
+				// The tree is now guaranteedly a right-right case.
 				rotate_left(root);
 			}
-		} else
-			throw std::exception();
-		root->_height = std::max(height(root->_left), height(root->_right)) + 1;
 
+		// If root's value is equal to inserted value, we have an exception.
+		// TODO: find a better exception to throw.
+		} else throw std::exception();
+
+		// Recalculate the node height according to the insertion.
+		root->_height = std::max(height(root->_left), height(root->_right)) + 1;
 		return root;
 	}
 
@@ -88,8 +108,11 @@ private:
 	}
 
 	node* remove(node* root, const T& item) {
+		// If we find a nullptr, the item does not exist in this tree.
 		if (root == nullptr)
 			throw std::exception();
+
+		// The same of insertion works here. Find where the item must be, rebalance if needed.
 		else if (root->_item > item) {
 			root->_left = remove(root->_left, item);
 			if (factor(root) == 2) {
@@ -99,39 +122,46 @@ private:
 			}
 		} else if (root->_item < item) {
 			root->_right = remove(root->_right, item);
+		
 			if (factor(root) == -2) {
 				if (factor(root->_right) == 1)
 					rotate_right(root->_right);
 				rotate_left(root);
 			}
-		} else {
+		}
+		
+		// If root's value is equal to removed value, we found the node to remove.
+		else {
+
+			// Leaf case: just delete the actual node.
 			if (root->_left == nullptr && root->_right == nullptr) {
 				delete root;
 				return nullptr;
 			}
 
-			// If there is right child
+			// If there is only right child, replace the to-be-deleted node and delete it.
 			if (root->_left == nullptr) {
 				node* aux = root->_right;
 				delete root;
 				return aux;
 			}
 
-			// If there is left child
+			// If there is only left child, replace the to-be-deleted node and delete it.
 			if (root->_right == nullptr) {
 				node* aux = root->_left;
 				delete root;
 				return aux;
 			}
 
-			// If there are both children
+			// If there are both children, find the immediately next value, swap and retry to remove.
 			node* aux = root->_right;
-			while (aux->_left != nullptr) {
+			while (aux->_left != nullptr)
 				aux = aux->_left;
-			}
 			std::swap(root->_item, aux->_item);
 			root->_right = remove(root->_right, item);
 		}
+
+		// Recalculate the node height according to the removal.
 		root->_height = std::max(height(root->_left), height(root->_right)) + 1;
 		return root;
 	}
@@ -161,6 +191,7 @@ private:
 	}
 
 	node* recursive_copy(node* other_root) {
+		// To recursively copy, create a new node, recursively copy it's left and right child, then return it to be attached.
 		node* aux = new node(other_root->_item);
 		aux->_left = recursive_copy(other_root->_left);
 		aux->_right = recursive_copy(other_root->_right);
@@ -168,6 +199,7 @@ private:
 	}
 
 	void recursive_delete(node* root) {
+		// To recursively delete, recursively delete both children if they exist, then delete.
 		if (root != nullptr) {
 			recursive_delete(root->_left);
 			recursive_delete(root->_right);
